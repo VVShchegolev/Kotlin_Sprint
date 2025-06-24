6be47d522e14d8835b553adfa7b893b857d20a21
VVShchegolev <VVShchegolev@>
2025-06-24 23:48:43 +1000

    KS-3-5 задача 1

---
+++ src/main/kotlin/Lesson_3_Ex_1/Lesson_3_Ex_1.kt
@@ -0,0 +1,14 @@
+package Lesson_3_Ex_1
+
+fun main() {
+
+    val userName = "Джонатан"
+    var helloText = "Добрый день"
+
+    println("$userName, $helloText!")
+
+    helloText = "Добрый вечер"
+
+    println("$userName, $helloText!")
+
+}
