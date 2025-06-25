81e528662d1e6bca001a221b57c8cf2f996b2a3e
VVShchegolev <VVShchegolev@>
2025-06-25 22:09:02 +1000

    KS-4-5 задача 3

---
+++ src/main/kotlin/Lesson_4_Ex_3/Lesson_4_Ex_3.kt
@@ -0,0 +1,14 @@
+package Lesson_4_Ex_3
+
+fun main() {
+
+    val isSunny: Boolean = true
+    val isTentOpen: Boolean = true
+    val humidity: Int = 20
+    val season: String = "зима"
+
+    val areConditionsFavorable = isSunny && isTentOpen && humidity == 20 && season != "зима"
+
+    println("Благоприятные ли условия сейчас для роста бобовых? $areConditionsFavorable")
+
+}
