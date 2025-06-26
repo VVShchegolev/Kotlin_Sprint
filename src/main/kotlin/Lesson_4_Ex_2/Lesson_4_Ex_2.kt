0463e48b3235381b84ed7f8c1e2a5b34d91845f9
YGD_SHIRO_VVS <youngsonik14@gmail.com>
2025-06-26 10:41:33 +1000

    Update Lesson_4_Ex_2.kt
    Исправленные недочеты:
    Добавил переменные "isAverage1" и "isAverage2" для условия, чтобы код можно было легче читать

--- src/main/kotlin/Lesson_4_Ex_2/Lesson_4_Ex_2.kt
+++ src/main/kotlin/Lesson_4_Ex_2/Lesson_4_Ex_2.kt
@@ -8,8 +8,11 @@ fun main() {
     val weight2 = 50
     val volume2 = 100
 
-    println("Груз с весом $weight1 кг и объемом $volume1 л соответствует категории 'Average': ${weight1 > MIN_WEIGHT_KG && weight1 <= MAX_WEIGHT_KG && volume1 < MAX_VOLUME_LITERS}")
-    println("Груз с весом $weight2 кг и объемом $volume2 л соответствует категории 'Average': ${weight2 > MIN_WEIGHT_KG && weight2 <= MAX_WEIGHT_KG && volume2 < MAX_VOLUME_LITERS}")
+    val isAverage1 = weight1 > MIN_WEIGHT_KG && weight1 <= MAX_WEIGHT_KG && volume1 < MAX_VOLUME_LITERS 
+    val isAverage2 = weight2 > MIN_WEIGHT_KG && weight2 <= MAX_WEIGHT_KG && volume2 < MAX_VOLUME_LITERS
+
+    println("Груз с весом $weight1 кг и объемом $volume1 л соответствует категории 'Average': $isAverage1")
+    println("Груз с весом $weight2 кг и объемом $volume2 л соответствует категории 'Average': $isAverage2")
 
 }
 
