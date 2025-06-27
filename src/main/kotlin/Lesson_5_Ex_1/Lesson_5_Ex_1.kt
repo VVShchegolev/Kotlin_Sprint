c7b977189b61b68905bd6690cfa33046aca89dc5
YGD_SHIRO_VVS <youngsonik14@gmail.com>
2025-06-27 10:53:06 +1000

    Update KS_5_1 Задача 1 исправлена
    Что исправлено, удалены константы и заменил захардженные значения на случайные числа из диапазона.random().

--- src/main/kotlin/Lesson_5_Ex_1/Lesson_5_Ex_1.kt
+++ src/main/kotlin/Lesson_5_Ex_1/Lesson_5_Ex_1.kt
@@ -3,15 +3,14 @@ package Lesson_5_Ex_1
 
 fun main() {
 
-    println("Чтобы доказать, что вы не бот, решите пример: $NUMBER1 + $NUMBER2 = ?")
+    val number1 = (1..10).random()
+    val number2 = (1..10).random()
+
+    println("Чтобы доказать, что вы не бот, решите пример: $number1 + $number2 = ?")
     println("Введите ответ:")
 
     val userAnswer = readln().toInt()
 
-    println(if (userAnswer == CORRECT_SUM) "Добро пожаловать!" else "Доступ запрещен.")
+    println(if (userAnswer == number1 + number2) "Добро пожаловать!" else "Доступ запрещен.")
 
 }
-
-const val NUMBER1 = 5
-const val NUMBER2 = 3
-const val CORRECT_SUM = NUMBER1 + NUMBER2
